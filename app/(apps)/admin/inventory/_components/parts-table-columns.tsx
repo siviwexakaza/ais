718fd46c14b5e31cf4a8fc558e9a389930bec6f7
sivnodlela <152187964+siviwe-asthat@users.noreply.github.com>
2025-05-02 01:25:43 +0200

    push table

--- app/(apps)/admin/inventory/_components/parts-table-columns.tsx
+++ app/(apps)/admin/inventory/_components/parts-table-columns.tsx
@@ -1,12 +1,32 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { ListBranchType, PartDetails } from "@/lib/types";
+import { PartDetails } from "@/lib/types";
 import { ColumnDef } from "@tanstack/react-table";
 import { ArrowUpDown } from "lucide-react";
 import Image from "next/image";
 
 export const PartTableColumns: ColumnDef<PartDetails>[] = [
+  {
+    accessorKey: "picture",
+    header: "Image",
+    cell: ({ row }) => {
+      const logoUrl = row.getValue("picture") as string;
+
+      return (
+        <div className="flex items-center justify-center">
+          <div className="relative w-12 h-12 rounded-md overflow-hidden">
+            <Image
+              src={logoUrl}
+              alt="Brand Logo"
+              fill
+              className="object-contain"
+            />
+          </div>
+        </div>
+      );
+    },
+  },
   {
     accessorKey: "name",
     header: ({ column }) => {
@@ -40,30 +60,27 @@ export const PartTableColumns: ColumnDef<PartDetails>[] = [
     header: "Branch",
   },
   {
+    accessorKey: "condition",
+    header: "Condition",
+  },
+  {
     accessorKey: "quantity",
-    header: "Quantity",
-  },
-  {
-    accessorKey: "price",
-    header: "Price",
-  },
-  {
-    accessorKey: "picture",
-    header: "",
+    header: "Status",
     cell: ({ row }) => {
-      const logoUrl = row.getValue("picture") as string;
+      const qty = parseInt(row.getValue("quantity"));
 
       return (
-        <div className="flex items-center justify-center">
-          <div className="relative w-12 h-12 rounded-md overflow-hidden">
-            <Image
-              src={logoUrl}
-              alt="Brand Logo"
-              fill
-              className="object-contain"
-            />
-          </div>
-        </div>
+        <>
+          {qty > 0 ? (
+            <div className="px-2 w-fit bg-green-300/40 text-green-700 rounded-md">
+              <p>In Stock</p>
+            </div>
+          ) : (
+            <div className="px-2 w-fit bg-red-300/40 text-red-700 rounded-md">
+              <p>Out of Stock</p>
+            </div>
+          )}
+        </>
       );
     },
   },
